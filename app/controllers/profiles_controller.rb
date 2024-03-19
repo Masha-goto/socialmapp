e1d56548d6033d540f042b3710f2cb8a7224ec52
masahiro <oosaki.production@gmail.com>
2024-03-19 14:47:47 +0900

    profileを作成する

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,30 @@
+class ProfilesController < ApplicationController
+	before_action :authenticate_user!
+
+	def show
+		@profile = current_user.profile
+	end
+
+	def edit
+		@profile = current_user.prepare_profile
+	end
+
+	def update
+		@profile = current_user.prepare_profile
+		@profile.assign_attributes(profile_params)
+		if @profile.save
+			redirect_to profile_path, notice: 'プロフィール作成できました!'
+		else
+			flash.now[:error] = '作成できませんでした'
+			render :edit
+		end
+	end
+
+	private
+	def profile_params
+		params.require(:profile).permit(
+			:account,
+			:avatar
+		)
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htmlz
 	root to: 'home#index'
+
+	resource :profile, only: %i(show edit update)
 end
