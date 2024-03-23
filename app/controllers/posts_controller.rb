fb3647cde581b005470639bbe188dd4b942f1336
masahiro <oosaki.production@gmail.com>
2024-03-23 17:05:32 +0900

    画像表示機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,7 +45,7 @@ class PostsController < ApplicationController
 
 	private
 	def post_params
-		params.require(:post).permit(:title, :content, :img)
+		params.require(:post).permit(:title, :content, :image, images: [])
 	end
 
 	def set_post
