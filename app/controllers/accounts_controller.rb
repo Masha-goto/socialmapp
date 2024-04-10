8ccdbec9dcec79685519f2978bae81396b5cb6d5
masahiro <oosaki.production@gmail.com>
2024-04-10 17:36:16 +0900

    フォロー機能実装

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,8 +1,23 @@
 class AccountsController < ApplicationController
-	def show
-		@user = User.find(params[:id])
-		if @user == current_user
+  before_action :set_user, only: [:followings, :followers]
+
+  def show
+    @user = User.find(params[:id])
+    if @user == current_user
       redirect_to profile_path
-		end
-	end
+    end
+  end
+
+  def followings
+    @users = @user.followings
+  end
+
+  def followers
+    @users = @user.followers
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
